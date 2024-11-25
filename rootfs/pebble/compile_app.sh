103e9adf1f216fbdf94c73904dea0b224e4622a8
Andrei Markeev <andrei-markeev@users.noreply.github.com>
2024-11-25 05:54:55 +0200

    add pypng and sh, rename build to assembled to avoid doubled build/build folders

--- rootfs/pebble/compile_app.sh
+++ rootfs/pebble/compile_app.sh
@@ -2,11 +2,11 @@ set -xe
 
 PATH=/pebble/arm-cs-tools/bin:$PATH
 
-mkdir -p /pebble/build
-cd /pebble/build
-mkdir -p /pebble/build/resources/fonts
-mkdir -p /pebble/build/resources/images
-mkdir -p /pebble/build/resources/data
+mkdir -p /pebble/assembled
+cd /pebble/assembled
+mkdir -p /pebble/assembled/resources/fonts
+mkdir -p /pebble/assembled/resources/images
+mkdir -p /pebble/assembled/resources/data
 cp -r /pebble/pebblejs/* .
 cp -r /pebble/app/* .
 python /pebble/sdk3/pebble/waf configure build
